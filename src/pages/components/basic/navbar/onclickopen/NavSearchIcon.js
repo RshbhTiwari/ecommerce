d0a4be7bedc7ce89f926c3f9b1d99bbd5b9ff964
RshbhTiwari <rishabh_tiwari@starreify.com>
2024-07-09 15:03:51 +0600

    cammit

--- src/pages/components/basic/navbar/onclickopen/NavSearchIcon.js
+++ src/pages/components/basic/navbar/onclickopen/NavSearchIcon.js
@@ -2,17 +2,35 @@ import { IoMdSearch } from "react-icons/io";
 import React, { useState, useEffect, useRef } from 'react';
 import { RxCross2 } from "react-icons/rx";
 import { Btnone } from "../../button";
-
 import { FaRegHeart } from "react-icons/fa";
 import { HiOutlineShoppingBag } from "react-icons/hi";
 import productcard from "../../../../../data/productcard";
 import { Paragraph } from "../../title";
+import { getproduct } from "../../../../../redux/slices/product";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 
 function NavSearchIcon() {
-    const [isSearchOpen, setIsSearchOpen] = useState(false);
+    const BASE_IMAGE_URL = 'http://127.0.0.1:8000/storage/';
     const searchInputRef = useRef(null);
+    const dispatch = useDispatch();
+    const [isSearchOpen, setIsSearchOpen] = useState(false);
+    const [allProductsData, setAllProductsData] = useState([]);
 
+    const { isLoading: productIsLoading, error: productError, products } = useSelector(
+        (state) => state.product
+    );
+
+    useEffect(() => {
+        dispatch(getproduct());
+    }, [dispatch]);
+
+    useEffect(() => {
+        if (products?.length) {
+            setAllProductsData(products);
+        }
+    }, [products]);
 
     useEffect(() => {
         if (isSearchOpen) {
@@ -28,6 +46,7 @@ function NavSearchIcon() {
     const toggleSearch = () => {
         setIsSearchOpen(!isSearchOpen);
     };
+
 
     return (
         <>
@@ -70,51 +89,70 @@ function NavSearchIcon() {
                             </div>
 
                             <div className="md:max-h-[250px] max-h-[158px] overflow-y-auto ">
-                                {productcard.map((item, index) => (
-                                    <div className="py-2 px-2 flex relative h-full items-center rounded-lg shadow-md"
-                                        key={index} >
+                                {allProductsData.map((item, index) => (
+                                    <a className="" href={`/shop/${item?.id}`}>
+                                        <div className={`grid grid-cols-12 gap-4 mb-2 py-2 px-2 flex relative h-full items-center rounded-lg shadow-md
+                                        ${index % 2 === 0 ? '' : 'bg-gray-200 '}`} key={index} >
+
+                                            <div key={index} className="lg:col-span-3 sm:col-span-4  col-span-12
+                                           flex items-center h-[150px] w-full justify-content rounded-md bg-[#00A762] p-3
+                                       sm:block hidden">
+
+                                                {item.additional_images && item.additional_images.length > 0 ? (
+                                                    <img
+                                                        src={BASE_IMAGE_URL + item.additional_images[0]}
+                                                        alt='product_img'
+                                                        className="w-full h-full object-cover rounded-md"
+                                                    />
+                                                ) : (
+                                                    <div className="w-full h-full bg-gray-200 rounded-md">
+
+                                                    </div>
+                                                )}
+                                            </div>
 
 
-                                        <div key={index} className="flex items-center w-[150px]  justify-content rounded-md bg-[#00a762b0] sm:block hidden">
-                                            <img
-                                                src={item.image}
-                                                alt='product_img'
-                                                className="w-full h-full"
-                                            />
-                                        </div>
+
+                                            <div className="col-span-12 sm:col-span-8 lg:col-span-9   flex justify-center flex-col">
+                                                <h2 className="text-[#00A762] text-left
+                                            font-dm text-lg capitalize font-medium
+                                           ">{item.name}</h2>
+
+                                                <Paragraph title={item.short_description} textAlign='onyleft' />
+
+                                                {item?.discount_price ? (
+                                                    <>
+                                                        <div className="flex items-center gap-2 text-[#00A762] text-center font-dm text-lg capitalize font-medium pb-2">
+                                                            <span className="block text-xs line-through">₹{item?.price}</span>
+                                                            <span className="block">₹{item?.discount_price}</span>
+
+                                                        </div>
+                                                    </>
+                                                ) : (
+                                                    <h2 className="text-[#00A762] font-dm text-lg capitalize font-medium pb-2">
+                                                        ₹{item?.price}
+                                                    </h2>
+                                                )}
+                                            </div>
 
 
-
-                                        <div className="flex justify-center flex-col sm:ml-3 ml-0">
-                                            <h2 className="text-[#00A762] md:text-left text-center
-                                                 font-dm text-lg capitalize font-medium
-                                                 ">{item.title}</h2>
-
-                                            <Paragraph title={item.subtitle} textAlign='left' />
-
-                                            <h2 className="text-[#00A762] md:text-left text-center
-                                                     font-dm text-lg capitalize font-medium
-                                                      ">${item.price}</h2>
-                                        </div>
-
-                                        <div className="absolute inset-0 flex items-center right-0 top-0 left-0 bottom-0
-                                          justify-center rounded-lg opacity-0 bg-[#00000040] hover:opacity-100 transition-opacity duration-300">
-                                            <div className="text-white text-center flex justify-center items-center gap-2">
-                                                <div className='flex justify-center w-10 h-10 rounded-lg items-center bg-[#072320] cursor-pointer '>
-                                                    <FaRegHeart className='text-white text-[22px]' />
-                                                </div>
-                                                <div className='flex justify-center w-10 h-10 rounded-lg items-center bg-[#072320] cursor-pointer'>
-                                                    <HiOutlineShoppingBag className='text-white text-[22px]' />
+                                            <div className="absolute inset-0 flex items-center right-0 top-0 left-0 bottom-0
+                                           justify-center rounded-lg opacity-0 bg-[#00000040] hover:opacity-100 transition-opacity duration-300">
+                                                <div className="text-white text-center flex justify-center items-center gap-2">
+                                                    <div className='flex justify-center w-10 h-10 rounded-lg items-center bg-[#072320] cursor-pointer '>
+                                                        <FaRegHeart className='text-white text-[22px]' />
+                                                    </div>
+                                                    <div className='flex justify-center w-10 h-10 rounded-lg items-center bg-[#072320] cursor-pointer'>
+                                                        <HiOutlineShoppingBag className='text-white text-[22px]' />
+                                                    </div>
                                                 </div>
                                             </div>
+
+
                                         </div>
-
-
-                                    </div>
+                                    </a>
                                 ))
                                 }
-
-
                             </div>
 
                             <div className="flex justify-center mt-4">
